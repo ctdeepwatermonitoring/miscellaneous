ea1dc2c862fbf1ba09ae1adb2e3d65bcf7388141
Mary Becker <mary.becker@ct.gov>
2023-02-26 10:36:24 -0500

    start itis taxa hierarchy grab

---
+++ itis.R
@@ -0,0 +1,20 @@
+library(jsonlite)
+library(stringr)
+
+# Get data for a particular taxon from ITIS
+data <- fromJSON("https://services.itis.gov/?q=tsn:3860")
+
+# Subset data to get hierarchy with ranks
+hier <- str_split(data$response$docs$hierarchySoFarWRanks[[1]],"[$]")
+
+# Get length of list
+length(hier[[1]])
+
+str_split(hier[[1]][1],":")[[1]][1]
+str_split(hier[[1]][1],":")[[1]][2]
+
+str_split(hier[[1]][2],":")[[1]][1]
+str_split(hier[[1]][2],":")[[1]][2]
+
+
+
